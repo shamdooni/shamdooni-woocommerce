1f844e0a969d9854fa3b035cc491a283ddba2b17
AjandM <aj.mardalizad@gmail.com>
2017-09-10 14:45:51 +0430

    database initialized

---
+++ includes/database.php
@@ -0,0 +1,28 @@
+<?php
+
+function jal_install() {
+    global $wpdb;
+    require_once( ABSPATH . 'wp-admin/includes/upgrade.php' );
+    global $charset_collate;
+    
+    $table_name = $wpdb->prefix . 'wc_shamdooni_transactions';
+	
+	$charset_collate = $wpdb->get_charset_collate();
+
+	$sql = "CREATE TABLE $table_name (
+		id mediumint(9) NOT NULL AUTO_INCREMENT,
+		time datetime DEFAULT '0000-00-00 00:00:00' NOT NULL,
+		session_key varchar(100) NOT NULL,
+		order_id int,
+		rounded boolean DEFAULT false,
+		final_amount bigint,
+		round_amount bigint,
+		amount bigint,
+        shamdooni_transaction_id varchar(100) NOT NULL,
+		PRIMARY KEY  (id)
+	) $charset_collate;";
+
+	dbDelta( $sql );
+
+}
+?>
---
+++ shamdooni.php
@@ -0,0 +1,13 @@
+<?php
+/*
+Plugin Name: شمعدونی ووکامرس
+Version: 1.0.0
+Description: رند کننده قیمت شمعدونی برای ووکامرس
+Plugin URI:
+Author: Shamdooni Organization
+Author URI: http://www.shamdooni.org
+*/
+
+
+
+?>
