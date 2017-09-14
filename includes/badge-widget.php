309a867aa9cebbea132f8bbc9405c7b3b8565152
AjandM <aj.mardalizad@gmail.com>
2017-09-14 20:07:46 +0430

    badge offline mode

--- includes/badge-widget.php
+++ includes/badge-widget.php
@@ -30,38 +30,40 @@ class wpb_widget extends WP_Widget {
 
         global $wc_shamdooni_address;
         
-        
-        $badge = wp_remote_get( $wc_shamdooni_address . '/api/rounder/v1/badge/key/'. $options['shamduni_apikey'], array( 'timeout' => 120, 'httpversion' => '1.1' ) );
-        $json = json_decode($badge['body']);
-        $badge_address = $json->{'badgeAddress'};
-        $badge_popup_address = $wc_shamdooni_address . '/api/site/badge/' . $json->{'userId'};
-        
+        if(is_shamdooni_up()) {
+            
+            $badge = wp_remote_get( $wc_shamdooni_address . '/api/rounder/v1/badge/key/'. $options['shamduni_apikey'], array( 'timeout' => 120, 'httpversion' => '1.1' ) );
+            $json = json_decode($badge['body']);
+            $badge_address = $json->{'badgeAddress'};
+            $badge_popup_address = $wc_shamdooni_address . '/api/site/badge/' . $json->{'userId'};
+            
 
-        $badge_html = '
-            <div id="shamdooni-badge" style="text-align: center; margin: 1em 0;" >
-                <a>
-                    <span style="
-                        display: inline-block;
-                        height: 100%;
-                        vertical-align: middle;
-                    "></span>
-                    <img src=" '. $json->{'badgeAddress'} .' " style="
-                        vertical-align: middle;
-                    "/>
-                </a>
-            </div>
-            <script>
-                var shamdooniBadge = document.getElementById("shamdooni-badge");            
-                shamdooniBadge.onclick = function() {                    
-                    window.open("'. $badge_popup_address .'", "Popup","toolbar=no, location=no, statusbar=no, menubar=no, scrollbars=1, resizable=0, width=541, height=640, top=30");
-                }; 
-            </script> 
-        ';
-        
-        echo $args['before_widget'];
-        echo $args['after_title'];
-        echo __( $badge_html, 'wpb_widget_domain' );
-        echo $args['after_widget'];
+            $badge_html = '
+                <div id="shamdooni-badge" style="text-align: center; margin: 1em 0;" >
+                    <a>
+                        <span style="
+                            display: inline-block;
+                            height: 100%;
+                            vertical-align: middle;
+                        "></span>
+                        <img src=" '. $json->{'badgeAddress'} .' " style="
+                            vertical-align: middle;
+                        "/>
+                    </a>
+                </div>
+                <script>
+                    var shamdooniBadge = document.getElementById("shamdooni-badge");            
+                    shamdooniBadge.onclick = function() {                    
+                        window.open("'. $badge_popup_address .'", "Popup","toolbar=no, location=no, statusbar=no, menubar=no, scrollbars=1, resizable=0, width=541, height=640, top=30");
+                    }; 
+                </script> 
+            ';
+            
+            echo $args['before_widget'];
+            echo $args['after_title'];
+            echo __( $badge_html, 'wpb_widget_domain' );
+            echo $args['after_widget']; 
+        }
     }
 } 
 
